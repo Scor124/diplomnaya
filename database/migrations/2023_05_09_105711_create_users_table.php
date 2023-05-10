5bf27d630c14ed1c5f17270e016511a27dea6e4b
Scor124 <andreylogan2@ya.ru>
2023-05-10 18:24:01 +0500

    Justmigrations

---
+++ database/migrations/2023_05_09_105711_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('DateOfBirth')->nullable();
+            $table->string('Phone', 12)->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('isTeacher')->nullable()->default(false);
+            $table->dateTime('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
